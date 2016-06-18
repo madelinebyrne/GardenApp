a1b8b93bdb972c157a16ea37bd6d4f55d22a2bc5
MadelineByrne <madelinebyrne@seattleacademy.org>
2016-06-17 17:12:08 -0700

    inital commit

---
+++ app/src/main/java/madelinebyrne/gardenapp/LOCATION.java
@@ -0,0 +1,10 @@
+package madelinebyrne.gardenapp;
+
+/**
+ * Created by Madeline Byrne on 6/17/2016.
+ */
+
+public class LOCATION
+{
+    //determine GPS or simple image
+}
---
+++ app/src/main/java/madelinebyrne/gardenapp/MainActivityTest.java
@@ -0,0 +1,13 @@
+package madelinebyrne.gardenapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_test);
+    }
+}
