423e4666e789473e6ceb9519ae0e253a058df674
MadelineByrne <madelinebyrne@seattleacademy.org>
2016-06-20 17:09:30 -0700

    created new field subclasses

--- app/src/main/java/madelinebyrne/gardenapp/GENUSFIELD.java
+++ app/src/main/java/madelinebyrne/gardenapp/GENUSFIELD.java
@@ -4,5 +4,14 @@ package madelinebyrne.gardenapp;
  * Created by Madeline Byrne on 6/20/2016.
  */
 
-public class GENUSFIELD extends FIELD {
+public class GenusFIELD extends FIELD
+{
+
+    //constructor
+    public GenusFIELD(String userInput)
+    {
+        fieldName="Genus";
+        this.fieldContent=userInput;
+        //generate ID
+    }
 }
