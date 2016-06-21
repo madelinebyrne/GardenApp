3d6d86bbb829c9f8ec1191f029c6a0cf637b66a6
MadelineByrne <madelinebyrne@seattleacademy.org>
2016-06-20 17:07:52 -0700

    added textbox input for basic testing

--- app/src/main/java/madelinebyrne/gardenapp/MainActivityTest.java
+++ app/src/main/java/madelinebyrne/gardenapp/MainActivityTest.java
@@ -2,6 +2,10 @@ package madelinebyrne.gardenapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivityTest extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivityTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_test);
+    }
+
+    public void inputEnter(View view)
+    {
+        EditText editTextInput = (EditText) findViewById(R.id.editText);
+        String test = editTextInput.getText().toString();
+        Log.d("test input = ", test);
     }
 }
