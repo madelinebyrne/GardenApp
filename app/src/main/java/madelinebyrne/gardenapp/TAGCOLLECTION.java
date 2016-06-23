026f06a5125c68e5fa436df626ced67a733f94ba
MadelineByrne <madelinebyrne@seattleacademy.org>
2016-06-23 15:27:16 -0700

    added tag collection to store/search through tags "

--- app/src/main/java/madelinebyrne/gardenapp/TAGCOLLECTION.java
+++ app/src/main/java/madelinebyrne/gardenapp/TAGCOLLECTION.java
@@ -1,8 +1,18 @@
 package madelinebyrne.gardenapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Madeline Byrne on 6/23/2016.
  */
 
-public class TAGCOLLECTION {
+public class TAGCOLLECTION
+{
+    public List<TAG> tagList = new ArrayList<TAG>();
+
+    public void addTag(TAG foo)
+    {
+        tagList.add(foo);
+    }
 }
