e48a950d6a0b9e61f7b79fbc6063543ab447f705
MadelineByrne <madelinebyrne@seattleacademy.org>
2016-06-22 17:48:03 -0700

    restructuring classes

--- app/src/main/java/madelinebyrne/gardenapp/MainActivityTest.java
+++ app/src/main/java/madelinebyrne/gardenapp/MainActivityTest.java
@@ -1,5 +1,6 @@
 package madelinebyrne.gardenapp;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,18 +8,38 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.List;
+
+
 public class MainActivityTest extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_test);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+
+    public TAG testTag;
     public void inputEnter(View view)
     {
         EditText editTextInput = (EditText) findViewById(R.id.editText);
         String test = editTextInput.getText().toString();
-        Log.d("test input = ", test);
+
     }
+
 }
