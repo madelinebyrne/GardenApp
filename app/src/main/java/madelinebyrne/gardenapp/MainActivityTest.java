b12b1b8b5045fc700f763dbf2a77a33c1f97f979
MadelineByrne <madelinebyrne@seattleacademy.org>
2016-06-23 17:22:44 -0700

    added test code for TAG functions (throws error)

--- app/src/main/java/madelinebyrne/gardenapp/MainActivityTest.java
+++ app/src/main/java/madelinebyrne/gardenapp/MainActivityTest.java
@@ -24,6 +24,9 @@ public class MainActivityTest extends AppCompatActivity {
      */
     private GoogleApiClient client;
 
+    public TAGCOLLECTION testTagCollection;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,15 +34,43 @@ public class MainActivityTest extends AppCompatActivity {
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-    }
+        testTagCollection.searchCommonName("daisy");
 
+    }
 
     public TAG testTag;
-    public void inputEnter(View view)
-    {
-        EditText editTextInput = (EditText) findViewById(R.id.editText);
-        String test = editTextInput.getText().toString();
 
+    EditText editTextInput;
+    public void inputUser(View view)
+    {
+       Log.d("inputUser", "entered");
+        editTextInput = (EditText) findViewById(R.id.editText);
+        String test = editTextInput.getText().toString();
+        Log.d("common name", test);
+        //testTag.setCommonName(test); // THIS LINE THROWS ERROR AND CAUSES APP TO CRASH (also code below throws errors)
+
+        //EditText editTextInputG = (EditText) findViewById(R.id.editText2);
+       // String GenusInput = editTextInputG.getText().toString();
+       // testTag.setGenus(GenusInput);
+//
+      //  EditText editTextInputS = (EditText) findViewById(R.id.editText3);
+      //  String SpeciesInput = editTextInputS.getText().toString();
+      //  testTag.setSpecies(SpeciesInput);
+       // testTagCollection.addTag(testTag);
+
+      //Log.d("testTag common name", testTagCollection.tagList.get(0).CommonName);
+      //  Log.d("testTag genus", testTagCollection.tagList.get(0).Genus);
+      //  Log.d("testTag species", testTagCollection.tagList.get(0).Species);
+       // String seasonalPlantC= testTagCollection.tagList.get(0).thisPlantSeasonalCharacteristics.toString();
+       // Log.d("testTag SPC", seasonalPlantC);
     }
 
+    public void evergreenClick()
+    {
+        testTag.setPlantSeasonalCharacterisesEvergreen();
+    }
+    public void deciduousClick()
+    {
+        testTag.setPlantSeasonalCharacteristicsDeciduous();
+    }
 }
