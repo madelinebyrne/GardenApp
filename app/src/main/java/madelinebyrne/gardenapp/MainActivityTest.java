6ddad0b8b1b6d903efab9db8e563b7626a3c18c7
MadelineByrne <madelinebyrne@seattleacademy.org>
2016-06-23 17:52:49 -0700

    fixed some bugs but Seasonal plant charaterisitics still doesn't work

--- app/src/main/java/madelinebyrne/gardenapp/MainActivityTest.java
+++ app/src/main/java/madelinebyrne/gardenapp/MainActivityTest.java
@@ -24,7 +24,7 @@ public class MainActivityTest extends AppCompatActivity {
      */
     private GoogleApiClient client;
 
-    public TAGCOLLECTION testTagCollection;
+    public TAGCOLLECTION testTagCollection = new TAGCOLLECTION();
 
 
     @Override
@@ -34,42 +34,44 @@ public class MainActivityTest extends AppCompatActivity {
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-        testTagCollection.searchCommonName("daisy");
 
     }
 
-    public TAG testTag;
+    public TAG testTag = new TAG();
 
     EditText editTextInput;
+    EditText editTextInputG;
+    EditText editTextInputS;
+    String seasonalPlantC;
     public void inputUser(View view)
     {
        Log.d("inputUser", "entered");
         editTextInput = (EditText) findViewById(R.id.editText);
         String test = editTextInput.getText().toString();
         Log.d("common name", test);
-        //testTag.setCommonName(test); // THIS LINE THROWS ERROR AND CAUSES APP TO CRASH (also code below throws errors)
+        testTag.setCommonName(test);
 
-        //EditText editTextInputG = (EditText) findViewById(R.id.editText2);
-       // String GenusInput = editTextInputG.getText().toString();
-       // testTag.setGenus(GenusInput);
-//
-      //  EditText editTextInputS = (EditText) findViewById(R.id.editText3);
-      //  String SpeciesInput = editTextInputS.getText().toString();
-      //  testTag.setSpecies(SpeciesInput);
-       // testTagCollection.addTag(testTag);
+        editTextInputG = (EditText) findViewById(R.id.editText2);
+        String GenusInput = editTextInputG.getText().toString();
+        testTag.setGenus(GenusInput);
 
-      //Log.d("testTag common name", testTagCollection.tagList.get(0).CommonName);
-      //  Log.d("testTag genus", testTagCollection.tagList.get(0).Genus);
-      //  Log.d("testTag species", testTagCollection.tagList.get(0).Species);
-       // String seasonalPlantC= testTagCollection.tagList.get(0).thisPlantSeasonalCharacteristics.toString();
+        editTextInputS = (EditText) findViewById(R.id.editText3);
+        String SpeciesInput = editTextInputS.getText().toString();
+        testTag.setSpecies(SpeciesInput);
+       testTagCollection.addTag(testTag);
+
+      Log.d("testTag common name", testTagCollection.tagList.get(0).CommonName);
+      Log.d("testTag genus", testTagCollection.tagList.get(0).Genus);
+        Log.d("testTag species", testTagCollection.tagList.get(0).Species);
+       // seasonalPlantC= testTagCollection.tagList.get(0).thisPlantSeasonalCharacteristics.toString(); //THROWS ERROR AND CRASHES APP AT THIS LINE
        // Log.d("testTag SPC", seasonalPlantC);
     }
 
-    public void evergreenClick()
+    public void evergreenClick() //THROWS ERROR AND CRASHES APP
     {
         testTag.setPlantSeasonalCharacterisesEvergreen();
     }
-    public void deciduousClick()
+    public void deciduousClick() //THROWS ERROR AND CRASHES APP
     {
         testTag.setPlantSeasonalCharacteristicsDeciduous();
     }
